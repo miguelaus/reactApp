52779a4aa8a60ff7015497dc8cc259241280cbbb
gitrodri <miguelaus@gmail.com>
2022-12-03 17:03:58 +0000

    correcion de fotos y algo de estilos a las cards

---
+++ src/Components/Images/Images.js
@@ -0,0 +1,33 @@
+import Margarita from "./margarita.jpg";
+import Caprese from "./caprese.jpg";
+import Primavera from "./primavera.jpg";
+import Cancha from "./cancha.jpg";
+import Pinta from "./pinta.jpg";
+import BotellaC from "./botellaC.jpg"
+import Copa from "./copa.jpg";
+import BotellaV from "./botellaV.jpg";
+import Fruta from "./fruta.jpg";
+import Helado from "./helado.jpg";
+import Tiramisu from "./tiramisu.jpg";
+import Flan from "./flan.jpg";
+
+
+
+export default {
+    "margarita": Margarita,
+    "caprese": Caprese,
+    "primavera": Primavera,
+    "cancha": Cancha,
+    "pinta": Pinta,
+    "botellaC": BotellaC,
+    "copa": Copa,
+    "botellaV": BotellaV,
+    "fruta": Fruta,
+    "Helado": Helado,
+    "tiramisu": Tiramisu,
+    "flan": Flan,
+
+
+
+
+}
--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,7 +1,5 @@
 import './ItemList.css';
-
-
-
+import Images from "../Images/Images"
 
 
 export default function ProductList() {
@@ -10,22 +8,85 @@ export default function ProductList() {
         id: 1,
         name: "Pizza Caprese",
         price: 300,
+        img: Images.caprese,
+        
       },
       {
         id: 2,
         name: "Pizza Margarita",
         price: 450,
+        img: Images.margarita,
       },
       {
         id: 3,
         name: "Pizza Primavera",
         price: 350,
+        img: Images.primavera,
       },
       {
         id: 4,
         name: "Pizza Cancha",
         price: 350,
+        img: Images.cancha,
+        
       },
+      {
+        id: 5,
+        name: "Cerveza pinta",
+        price: 350,
+        img: Images.pinta,
+        
+      },
+      {
+        id: 6,
+        name: "Cerveza Botella",
+        price: 350,
+        img: Images.botellaC,
+        
+      },
+      {
+        id: 7,
+        name: "Vino Copa",
+        price: 350,
+        img: Images.copa,
+        
+      },
+      {
+        id: 8,
+        name: "Vino Botella",
+        price: 350,
+        img: Images.botellaV,
+        
+      },
+      {
+        id: 9,
+        name: "Flan",
+        price: 350,
+        img: Images.flan,
+        
+      },
+      {
+        id: 10,
+        name: "Fruta",
+        price: 350,
+        img: Images.fruta,
+        
+      },
+      {
+        id: 11,
+        name: "Helado",
+        price: 350,
+        img: Images.Helado,
+        
+      },
+      {
+        id: 12,
+        name: "Tiramisu",
+        price: 350,
+        img: Images.tiramisu,
+        
+      },
+
 
     ];
 
@@ -34,14 +95,15 @@ export default function ProductList() {
 
   
     return (
-      <div className="product-list__container d-flex">
-        <div className="text-black card" >
+      <div className="product-list__container d-flex mt-5 height">
+        <div className="text-black card mt-3 " >
           
           {products.map((product) => (
             <div className="card body">
-              <div className ="my-2 " key={product.id}>
+              <div className ="my-3" key={product.id}>
               <h5 className="card-title">{product.name} </h5>  Price: {product.price}
               <button id="agregarBtn${prod.id}" className="btn btn-primary">Agregar al Carrito</button>
+              <img src={product.img} alt="Producto"></img>
               </div>
             </div>
           ))}
