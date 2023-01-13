bdeb59d95ed7676c2f6679b31fdd1ffac5fbec2f
gitrodri <miguelaus@gmail.com>
2023-01-13 17:15:01 +0000

    firebase

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,21 +1,28 @@
-
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import { getProduct } from "../../Data/Products";
-
+import { collection, doc, getDoc } from 'firebase/firestore'
+import { database } from "../../Firebase/config";
 const ItemDetailContainer = () => { 
     const [product, setProduct] = useState({})
 
     const {itemId} = useParams(); 
 
-    useEffect(() => {
-                               
-                    getProduct(itemId)
-                    .then((product) => setProduct(product) )
-                },[itemId]);
-
-
+    useEffect(()=>{
+      //le decimos nuestra base de datos y en que collecion tiene que ir
+      const coleccionProd = collection(database, "items")
+      // hacer una referencia que me traiga el ID del useParams
+      const referenciaDoc = doc(coleccionProd, itemId)
+      //traemos el documento
+      getDoc(referenciaDoc)
+      .then((result)=>{
+        setProduct({
+          id:result.id,
+          ...result.data()
+        })
+      })
+      .catch((error)=> console.log(error))
+    }, [itemId])
    return  (
      <ItemDetail product={product} />
    );
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,8 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
-import { getProducts } from "../../Data/Products";
-import { getProductsByCategory } from "../../Data/Products";
 import ItemList from "../ItemList/ItemList";
 import { database } from "../../Firebase/config";
-import { doc, getDoc } from "../../Firebase/firestore"
-import { collection, getDocs, query, where } from '../../Firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore'
 
 const ItemListContainer = () => { 
     const [products, setProducts] = useState([])
